74e02646fd3f4267d27399d848414e4476931083
hupratt <hprattdo@gmail.com>
2023-02-21 14:36:26 +0100

    remove env from git

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ from .seeds import seed_commands
 
 from .config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="react-app/build")
 
 # Setup login manager
 login = LoginManager(app)
