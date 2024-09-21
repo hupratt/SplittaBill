cf2293ba174a01d1e56b444d5f2073fcabb5d071
hupratt <hprattdo@gmail.com>
2024-09-21 19:35:20 +0200

    minio capable

--- app/aws_s3.py
+++ app/aws_s3.py
@@ -8,6 +8,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=os.environ.get("S3_KEY"),
     aws_secret_access_key=os.environ.get("S3_SECRET"),
+    endpoint_url=os.environ.get("S3_CUSTOM_URL")
 )
 
 ALLOWED_EXTENTIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
@@ -22,20 +23,16 @@ def get_unique_filename(filename):
 
 
 BUCKET_NAME = os.environ.get("S3_BUCKET")
-S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
+S3_LOCATION = f"https://minio-api.thekor.eu/{BUCKET_NAME}/"
 
-def upload_file_to_s3(file, acl="public-read"):
+def upload_file_to_s3(file):
     print(f"file: {file}, bucket: {BUCKET_NAME}, location: {S3_LOCATION}")
 
     try:
-        s3.upload_fileobj(
+        s3.upload_file(
             file,
             BUCKET_NAME,
-            file.filename,
-            ExtraArgs={
-                "ACL": acl,
-                "ContentType": file.content_type
-            }
+            file.filename
         )
     except Exception as e:
         return {"errors": str(e)}
